a7b2db774aa5ad130a710b20f8ca524783e87677
Luis David Choque Castro <moonlightvisitor@outlook.com>
2019-09-26 12:41:16 -0500

    Update install_apache2_php7.sh

--- install_apache2_php7.sh
+++ install_apache2_php7.sh
@@ -1,14 +1,6 @@
 # Install:
 
-sudo apt-get install apache2 php7.x libapache2-mod-php7.x 
-
-# Verify:
-
-a2query -m php7.x
-
-# Load:
-
-sudo a2enmod php7.x
+sudo apt-get install apache2 php libapache2-mod-php
 
 # Restart apache:
 
